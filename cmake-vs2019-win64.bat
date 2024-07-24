d2ec10af9052c44c373d18a8ffa34cff908fa438
Robert Beckebans <robert.beckebans@gmail.com>
2024-07-24 13:59:57 +0200

    Fixed vcpkg related problems on Windows

--- cmake-vs2019-win64.bat
+++ cmake-vs2019-win64.bat
@@ -2,5 +2,5 @@ REM cd ..
 del /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019_64" ..
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_MODULE_PATH="cmake/packages" -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019_64" ..
 pause
