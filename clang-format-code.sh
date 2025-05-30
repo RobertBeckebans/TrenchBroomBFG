e8c5d5b6577a8249200e30da69814c48619c6192
Robert Beckebans <robert.beckebans@gmail.com>
2025-05-30 17:03:40 +0200

    Merged clang-format-code.sh from RBDoom

--- clang-format-code.sh
+++ clang-format-code.sh
@@ -1,41 +1,63 @@
 #!/bin/sh
 
-# keep the following in sync with our supplied clang-format binaries!
-OUR_CLANGFMT_VERSION=13.0.1
+# Desired clang-format version
+REQUIRED_VERSION=18.1.8
 
 print_usage () {
-	#echo "By default, this script only works on Linux i686 or amd64 (with our supplied clang-format binaries)"
-	#echo "You can use your own clang-format binary by setting the CLANGFMT_BIN environment variable before calling this script"
-	#echo "  e.g.: CLANGFMT_BIN=/usr/bin/clang-format $0"
-	echo "But please make sure it's version $OUR_CLANGFMT_VERSION because other versions may format differently!"
+    echo "Please ensure that clang-format version $REQUIRED_VERSION is used, as other versions may produce different formatting!"
+    echo "Set the CLANGFMT_BIN environment variable to specify a custom path to clang-format."
+    echo "Example: CLANGFMT_BIN=/usr/bin/clang-format $0"
 }
 
-#if [ -z "$CLANGFMT_BIN" ]; then
-#
-#	if [ `uname -s` != "Linux" ]; then
-#		print_usage
-#		exit 1
-#	fi
+# Determine platform and set clang-format binary path
+case "$(uname -s)" in
+    Linux*)   CLANGFMT_DEFAULT="./clang-format" ;;
+    MINGW*|MSYS*|CYGWIN*) CLANGFMT_DEFAULT="./clang-format.exe" ;;
+    *)        echo "ERROR: Unsupported platform: $(uname -s)"; print_usage; exit 1 ;;
+esac
 
-#	case "`uname -m`" in
-#		i?86 | x86 ) CLANGFMT_SUFFIX="x86" ;;
-#		amd64 | x86_64 ) CLANGFMT_SUFFIX="x86_64" ;;
-#		* ) print_usage ; exit 1  ;;
-#	esac
+# Use custom path or default path
+CLANGFMT_BIN=${CLANGFMT_BIN:-$CLANGFMT_DEFAULT}
 
-#	CLANGFMT_BIN="./clang-format.$CLANGFMT_SUFFIX"
-#fi
-
-CLANGFMT_BIN=./clang-format.exe
-
-#CLANGFMT_VERSION=$($CLANGFMT_BIN --version | grep -o -e "[[:digit:]\.]*")
-CLANGFMT_VERSION=$($CLANGFMT_BIN --version | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")
-
-if [ "$CLANGFMT_VERSION" != "$OUR_CLANGFMT_VERSION" ]; then
-	echo "ERROR: $CLANGFMT_BIN has version $CLANGFMT_VERSION, but we want $OUR_CLANGFMT_VERSION"
-	echo "       (Unfortunately, different versions of clang-format produce slightly different formatting.)"
-	exit 1
+# Check if the binary exists and is executable
+if [ ! -x "$CLANGFMT_BIN" ]; then
+    echo "ERROR: $CLANGFMT_BIN not found or not executable"
+    print_usage
+    exit 1
 fi
 
+# Extract clang-format version, take only the first match
+CLANGFMT_VERSION="$($CLANGFMT_BIN --version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)"
+if [ -z "$CLANGFMT_VERSION" ]; then
+    echo "ERROR: Could not extract version from $CLANGFMT_BIN --version"
+    exit 1
+fi
+
+# Clean the version strings
+CLANGFMT_VERSION_CLEAN="$(echo "$CLANGFMT_VERSION" | tr -d '\r\n[:space:]' | sed 's/[^0-9.]//g')"
+REQUIRED_VERSION_CLEAN="$(echo "$REQUIRED_VERSION" | tr -d '\r\n[:space:]' | sed 's/[^0-9.]//g')"
+
+# Debug output to inspect values
+#echo "DEBUG: CLANGFMT_BIN: $CLANGFMT_BIN"
+#echo "DEBUG: Raw CLANGFMT_VERSION: '$CLANGFMT_VERSION'"
+#echo "DEBUG: Cleaned CLANGFMT_VERSION_CLEAN: '$CLANGFMT_VERSION_CLEAN'"
+#echo "DEBUG: Cleaned REQUIRED_VERSION_CLEAN: '$REQUIRED_VERSION_CLEAN'"
+
+# Compare versions
+if [ "$CLANGFMT_VERSION_CLEAN" != "$REQUIRED_VERSION_CLEAN" ]; then
+    echo "ERROR: $CLANGFMT_BIN has version $CLANGFMT_VERSION, but version $REQUIRED_VERSION is required"
+    echo "       (Different versions of clang-format may produce slightly different formatting.)"
+    exit 1
+fi
+
+# Check for multiple .clang-format files
+CLANG_FORMAT_FILES=$(find . -name ".clang-format" | wc -l)
+if [ "$CLANG_FORMAT_FILES" -gt 1 ]; then
+    echo "WARNING: Multiple .clang-format files found in the project directory:"
+    find . -name ".clang-format"
+    echo "This may cause conflicts. Consider keeping only one .clang-format file in the project root."
+fi
+
+echo "Formatting completed!"
 #find . -regex ".*\.\(c\|cpp\|cc\|cxx\|h\|hpp\)" ! -path "./lib/*" ! -exec $CLANGFMT_BIN -i {} \;
 find . -regex ".*\.\(c\|cpp\|cc\|cxx\|h\|hpp\)" ! -path "./lib/*" ! -path "./vcpkg/*" ! -path "./build/*" ! -print0 | xargs -0 -P 16 $CLANGFMT_BIN -i
